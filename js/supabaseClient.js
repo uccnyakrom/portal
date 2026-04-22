a64c56ae08d218b25e861e4da8b2645ff3fbc58e
uccnyakrom <ucc.nyakrom@outlook.com>
2026-04-22 16:01:16 +0000

    Delete js directory

--- js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://rgjaceihgcvhvqrisxew.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnamFjZWloZ2N2aHZxcmlzeGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MzMwNzQsImV4cCI6MjA5MjQwOTA3NH0.sBkNWbxqQgAAcaqI86qVD7d0KLChQigbUmq4-C1diFE"; // paste yours
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
